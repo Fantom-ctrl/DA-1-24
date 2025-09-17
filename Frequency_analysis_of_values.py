eb33d7a0255b00a65a2438c5d6e0014b509eb6ab
Fantom-ctrl <mara.sirazieva@yandex.ru>
2025-09-17 13:04:02 +0700

    Добавление проверок на ошибки

--- Frequency_analysis_of_values.py
+++ Frequency_analysis_of_values.py
@@ -12,6 +12,11 @@ def chart(count_frequency):
     Возврат:
     Функция отображает график, но не возвращает значения.
     """
+    # Проверка на правильный тип данных 
+    if not isinstance(count_frequency, p.Series):
+        print("В функцию передан не тот тип данных")
+        return
+    
     count_frequency.plot(kind="barh", color="blue", edgecolor="black")
     plt.title("Частота значений")
     plt.xlabel("Доля")
@@ -26,8 +31,18 @@ def main():
             "Red", "Red", "Red", "Green",
             "Green", "Blue", "Blue", "Blue"]}
     
+    # Проверкана то, что словарь не пуст
+    if not data:
+        print("Нет данных")
+        return
+    
     # Построение таблицы
     table = p.DataFrame(data)
+
+    # Проверка на то, что таблица не пустая
+    if table.empty:
+        print("Таблица пуста")
+        return
 
     # Подсчитываем частоту
     count_frequency = table["Category"].value_counts(normalize=True)
